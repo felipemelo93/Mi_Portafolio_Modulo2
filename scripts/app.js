cda7fad07eeb5c7dcf2ad11692210d4cafa5e2ac
felipe melo <felipemelo9318.fm@gmail.com>
2024-06-23 20:07:29 -0500

    Entrega final de mi portafolio

---
+++ scripts/app.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar', // Cambia esto a 'line', 'pie', etc., según el tipo de gráfica que prefieras
+        data: {
+            labels: ['Redes Sociales', 'Soporte Técnico', 'Bases de Datos', 'Desarrollo Web', 'Power Bi'],
+            datasets: [{
+                label: 'Nivel de Experiencia',
+                data: [80, 85, 95, 30, 30], // Valores de experiencia en porcentajes
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+});
+
+
+
+    
